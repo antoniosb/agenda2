72c9051148d73500a26fe2ec173b410597aeaea9
Antonio Augusto <antonioabritto@gmail.com>
2014-09-25 02:00:27 -0300

    just a reminder of heroku job

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,5 @@ set :output, "/home/antonio/Public/appointments/agenda2/log/cron_log.log"
 every :day, :at => '12:05 am' do  
   runner "Appointment.set_overdue_appointments"
 end
+#Heroku
+#bundle exec rails runner -e production "Appointment.set_overdue_appointments"
